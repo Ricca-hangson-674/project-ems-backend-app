43c36fe122275c99b9a79c2a74c20a4a4c37f2e3
nrh674 <n.riccahang674@gmail.com>
2024-09-12 19:49:05 +0400

    initial 12 Sept 2024 19:50

---
+++ ems-backend-app/src/test/java/com/riccahang/ems/EmsBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riccahang.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
